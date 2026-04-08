89818605a0b3cd6779bfb9ae1ce6ab9e2df113ce
junbownow <junbownow@gmail.co.jp>
2026-04-08 20:47:30 +0900

    モーダルdialog実装を追加

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// モーダル（div実装）
 // 必要な要素の取得
 const openModalDiv = document.getElementById('open-modal-div');
 const closeModalDiv = document.getElementById('close-modal-div');
@@ -22,4 +23,28 @@ overlayDiv.addEventListener('click', () => {
 // モーダルボックスないクリックでは閉じない
 modalDivBox.addEventListener('click', (e) => {
   e.stopPropagation();
+});
+
+// モーダル（dialog実装）
+// 必要な要素の取得
+const openModalDialog = document.getElementById('open-modal-dialog');
+const closeModalDialog = document.getElementById('close-modal-dialog');
+const dialogBox = document.getElementById('dialog-box');
+
+// 開く
+openModalDialog.addEventListener('click', () => {
+  dialogBox.showModal();
+});
+
+// 閉じる(ボタン)
+closeModalDialog.addEventListener('click', () => {
+  dialogBox.close();
+});
+
+// 閉じる(::backdropクリックで閉じる)
+dialogBox.addEventListener('click', (e) => {
+  console.log(e.target);
+  if (e.target === dialogBox) {
+    dialogBox.close();
+  }
 });
